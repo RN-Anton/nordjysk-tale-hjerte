73afa62faee4ef4bb2a27376eb37444e0d27362a
Anton Larsen <anton.larsen@rn.dk>
2026-03-02 14:50:53 +0100

    til test

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://talebesked.ai.rn.dk/call";
-export const API_KEY = import.meta.env.API_KEY || "";
+export const API_BASE_URL = "https://talebesked.ai.rn.dk/call";
+export const API_KEY = "f6547e66932def6df5722ea96447de18f3d996cf0b7721c8c735a4fd64a61233";
+
+
